f3a6af3703fdfa4e791d88cd21f62a0781f833bd
GIGA <bhavitkanthalia16@gmail.com>
2021-05-03 22:18:41 +0530

    logo changed and animation

--- public/js/main.js
+++ public/js/main.js
@@ -29,29 +29,29 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 2000,
+    duration: 1000,
     reset: true
 });
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 100}); 
-sr.reveal('.home__img',{delay: 100}); 
-sr.reveal('.home__social-icon',{ interval: 100}); 
+//sr.reveal('.button',{delay: 100}); 
+sr.reveal('.home__img',{delay: 50}); 
+sr.reveal('.home__social-icon',{ interval: 50}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 100}); 
-sr.reveal('.about__text',{delay: 100}); 
+sr.reveal('.about__subtitle',{delay: 50}); 
+sr.reveal('.about__text',{delay: 50}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 100}); 
-sr.reveal('.skills__img',{delay: 100});
+sr.reveal('.skills__data',{interval: 50}); 
+sr.reveal('.skills__img',{delay: 50});
 
 /*SCROLL WORK*/
-//sr.reveal('.work__img',{interval: 100}); 
+sr.reveal('.work__img',{interval: 50}); 
 
 /*SCROLL CONTACT*/
-//sr.reveal('.contact__input',{interval: 100}); 
+sr.reveal('.contact__input',{interval: 50}); 
